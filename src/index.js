a35f1435084a0f33512843720550ef160ba55310
Mauricio <jourdanmauricio@gmail.com>
2022-10-23 04:15:31 -0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,104 +1,70 @@
-const form = document.querySelector('#form');
-const button = document.querySelector('#submit-button');
-const list = document.querySelector('#list');
+const form = document.getElementById('form');
+const button = document.getElementById('submit-button');
+const list = document.getElementById('list');
 const onlineList = document.querySelector('#online-list');
+const onlineButton = document.querySelector('#online-button');
 const localButton = document.querySelector('#local-button');
-const price = form.price;
 const name = form.name;
-
-// TO DO
-
-// COMPLETE DRAG AND DROP
-// FETCH TO FAKE STORE API: https://api.escuelajs.co/api/v1/products
-
-let productList;
-
-if (getLocalStorage()) {
-  productList = getLocalStorage();
-} else {
-  productList = [];
-}
-
-const nodesItem = [];
+const price = form.price;
+let productList = [];
+const nodesItems = [];
 const onlineListArray = [];
+
+localButton.addEventListener('click', clearLocalStorage);
+onlineButton.addEventListener('click', () =>
+  fetchData(`${API}?limit=10&offset=10`)
+);
+
+const API = 'https://api.escuelajs.co/api/v1/products';
 
 let startPosition;
 let finalPosition;
+
+if (getLocalStorage()) {
+  productList = getLocalStorage();
+}
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   productList.push({ name: name.value, price: price.value });
   window.localStorage.setItem('local', JSON.stringify(productList));
-  createItem(name.value, price.value, list, nodesItem);
+  createItem(name.value, price.value, list, nodesItems);
   cleanInputs();
 });
 
 function getLocalStorage() {
   let obj = JSON.parse(window.localStorage.getItem('local'));
-  console.log(obj);
   return obj;
+}
+
+function createItem(name, price, container, array) {
+  if (name === '' || price === '') return;
+
+  const li = document.createElement('li');
+  li.classList.add('list--item');
+
+  li.setAttribute('data-index', array.length);
+
+  li.innerHTML = `
+    <span>${array.length}</span>
+    <div class='item--draggable' draggable='true'>
+      <p>${name}</p>
+      <p>${price}$</p>
+      <span>...</span>
+    </div>
+`;
+  array.push(li);
+  container.appendChild(li);
+  addEvents(li);
 }
 
 function clearLocalStorage() {
   window.localStorage.clear();
 }
 
-localButton.addEventListener('click', clearLocalStorage());
-
 function cleanInputs() {
-  price.value = '';
   name.value = '';
-}
-
-function createItem(name, price, container, array) {
-  const li = document.createElement('li');
-  li.classList.add('list--item');
-  li.setAttribute('data-index', array.length);
-
-  li.innerHTML = `
-    <span>${array.length}</span>
-    <div class="item--draggable" draggable="true">
-      <p>${name}</p>
-      <p>${price}$</p>
-      <span>...</span>
-    </div>
-  `;
-
-  array.push(li);
-  container.appendChild(li);
-  addEvents(li);
-}
-
-function dragStart() {
-  startPosition = Number(this.closest('li').getAttribute('data-index'));
-}
-
-function dragOver(e) {
-  e.preventDefault();
-}
-function dragEnter() {
-  this.classList.add('over');
-}
-function dragLeave() {
-  this.classList.remove('over');
-}
-function dragDrop() {
-  finalPosition = Number(this.getAttribute('data-index'));
-  swapItems(startPosition, finalPosition);
-}
-
-function swapItems(initialPosition, finalPosition) {
-  const firstElement =
-    nodesItem[initialPosition].querySelector('.item--draggable');
-  const secondElement =
-    nodesItem[finalPosition].querySelector('.item--draggable');
-
-  nodesItem[initialPosition].appendChild(secondElement);
-  nodesItem[finalPosition].appendChild(firstElement);
-
-  if (nodesItem[finalPosition].classList.contains('over')) {
-    nodesItem[finalPosition].classList.remove('over');
-  }
+  price.value = '';
 }
 
 function addEvents(node) {
@@ -111,13 +77,44 @@ function addEvents(node) {
   node.addEventListener('dragleave', dragLeave);
 }
 
-// PONER ESTE CODIGO EN OTRO ARCHIVO JS
+function dragStart() {
+  startPosition = Number(this.closest('li').getAttribute('data-index'));
+}
 
-const API = 'https://api.escuelajs.co/api/v1/products';
-const onlineButton = document.querySelector('#online-button');
+function dragDrop() {
+  finalPosition = Number(this.getAttribute('data-index'));
+  swapItems(startPosition, finalPosition);
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter() {
+  //   console.log('dragEnter');
+  this.classList.add('over');
+}
+function dragLeave() {
+  this.classList.remove('over');
+  //  console.log('dragLeave');
+}
+
+function swapItems(initialPosition, finalPosition) {
+  console.log(initialPosition, finalPosition);
+  const firstElement =
+    nodesItems[initialPosition].querySelector('.item--draggable');
+  const secondElement =
+    nodesItems[finalPosition].querySelector('.item--draggable');
+
+  nodesItems[initialPosition].appendChild(secondElement);
+  nodesItems[finalPosition].appendChild(firstElement);
+
+  if (nodesItems[finalPosition].classList.contains('over')) {
+    nodesItems[finalPosition].classList.remove('over');
+  }
+}
 
 function fetchData(APIWithQuery) {
-  // CAMBIAR A ASYNC AWAIT
   fetch(APIWithQuery)
     .then((data) => data.json())
     .then((products) => {
@@ -127,8 +124,7 @@ function fetchData(APIWithQuery) {
     });
 }
 
-fetchData(`${API}?offset=0&limit=10`);
-
-onlineButton.addEventListener('click', () => {
-  fetchData(`${API}?offset=5&limit=15`);
-});
+fetchData(`${API}?limit=10&offset=0`);
+// for (let i = 0; i < 10; i++) {
+//   createItem(`Product${i}`, i * 1000);
+// }
